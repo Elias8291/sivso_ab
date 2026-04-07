6c99c749cdaca353ee974fb7aec2858f9ff1d8bf
Abisai <69944428+Elias8291@users.noreply.github.com>
2026-04-07 12:08:40 -0600

    AB

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="min-h-dvh">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="min-h-dvh notranslate" translate="no">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="google" content="notranslate">
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
